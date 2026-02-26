586751fd5de68a2577e6f6a46857b8143a4a0db7
Maxim Krynyckyj <kosmopakowastaken@gmail.com>
2026-02-26 09:50:53 +0100

    Add Hero component, get rid of unused things

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+export const Hero = () => {
+    return (
+        <section className="h-screen flex flex-col text-center items-center justify-center bg-main1">
+            <h1 className="font-main text-4xl font-bold text-main2" >
+            MAXIM KRYNYCKYJ
+            </h1>
+            <h2 className="font-sec text-2xl font-normal text-main2">
+            front-end developer
+            </h2>
+        </section>
+    );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ['"Montserrat"', 'sans-serif'], 
+        sec: ['"Blinker"', 'sans-serif'], 
+      },
+      colors: {
+        main1: '#EBE5C2',
+        main2: '#504B38',
+        sec1: '#F8F3D9',
+        sec2: '#B9B28A'
+      }
+    },
   },
   plugins: [],
 }
