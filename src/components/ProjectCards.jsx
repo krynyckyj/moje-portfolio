dbc103ca946200b70f2f63091eccc0a4b24a0173
Maxim Krynyckyj <kosmopakowastaken@gmail.com>
2026-03-30 15:50:47 +0200

    Spotlight card and project card editing

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,22 +1,26 @@
 import { useTranslation } from 'react-i18next';
 import React from 'react';
 import SpotlightCard from './SpotlightCard';
+import { Link } from 'react-router-dom';
 
 
 export const ProjectCards = () => {
     const { t } = useTranslation();
 
     return (
-                             <SpotlightCard className="custom-spotlight-card group" spotlightColor="rgba(0, 229, 255, 0.2)">
-                                 <h3 className="text-2xl text-main2 font-bold relative z-10">{t('skills.title_one')}</h3>
-                                      <p className="text-zinc-400 relative z-10">{t('skills.info_one')}</p>
+                        <Link to="https://kuchyne-srubar.cz/" target="_blank" rel="noopener noreferrer" className="block-w-full">
+                             <SpotlightCard className=" cursor-pointer custom-spotlight-card group" spotlightColor="rgb(98, 89, 73)">
+                             <div className="gap-4 flex flex-col">
+                                 <h3 className="text-4xl text-main2 font-bold relative  tracking-tight leading-tight drop-shadow-[0_0_20px_rgba(255,255,255,0.4)]">{t('skills.title_one')}</h3>
+                                      <p className="text-zinc-400 text-xl relative  tracking-tight leading-tight drop-shadow-[0_0_20px_rgba(255,255,255,0.4)]">{t('skills.info_one')}</p>
+                              </div>
                                         <div className="absolute bottom-0 md:-bottom-10 left-0 w-full px-8 transition-transform duration-300 ease-in-out transform group-hover:-translate-y-2 md:group-hover:-translate-y-4">
                                             <img 
                                             src="/kuchyne-dark.jpg" 
                                             alt="Kuchyně Šrubař ukázka" 
                                             className="rounded-md" />
                                         </div>
-
                             </SpotlightCard>
+                        </Link>
     );
 };
--- src/components/SpotlightCard.jsx
+++ src/components/SpotlightCard.jsx
@@ -42,10 +42,9 @@ const SpotlightCard = ({ children, className = '', spotlightColor = 'rgba(255, 2
       className={`min-h-[350px] md:min-h-[750px] relative rounded-3xl border-4 border-neutral-800 bg-neutral-900 overflow-hidden p-8 ${className}`}
     >
       <div
-        className="pointer-events-none absolute inset-0 opacity-0 transition-opacity duration-300 ease-in-out"
+        className=" absolute inset-0 transition-opacity duration-300 opacity-100 md:opacity-0 md:group-hover:opacity-100"
         style={{
-          opacity,
-          background: `radial-gradient(circle at ${position.x}px ${position.y}px, ${spotlightColor}, transparent 80%)`
+          background: `radial-gradient(circle at ${position.x ? `${position.x}px` : '50%'} ${position.y ? `${position.y}px` : '50%'}, ${spotlightColor}, transparent 80%)`
         }}
       />
       {children}
